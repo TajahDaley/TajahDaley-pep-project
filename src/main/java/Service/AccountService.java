b4376c4484f37d6afea8c47e398ecde88708304c
Tajah Daley <tajdaley@yahoo.com>
2023-02-13 16:59:34 +0000

    update Service

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,15 +1,30 @@
 package Service;
 
+import DAO.AccountDAO;
 import Model.Account;
 
 public class AccountService {
+    private AccountDAO accountDAO;
+
+    public AccountService() {
+        accountDAO = new AccountDAO();
+    }
+
+    public AccountService(AccountDAO accountDAO) {
+        this.accountDAO = accountDAO;
+    }
 
     public Account addAccount(Account account) {
-        return null;
+        if(account.username != "" && account.password.length() >= 4) {
+            return accountDAO.addAccount(account);
+        } else {
+            return null;
+        }
+
     }
 
     public Account login(Account account) {
-        return null;
+        return accountDAO.loginAccount(account);
     }
     
 }

6a25eb709da6d0bbcf06b4a13a13d5daaa4f6f30
Tajah Daley <tajdaley@yahoo.com>
2023-02-14 17:59:12 +0000

    One test left to pass

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -17,7 +17,7 @@ public class MessageDAO {
     public Message addMessage(Message message) { 
         Connection connection = ConnectionUtil.getConnection();
         try {
-            String sql = "INSERT INTO message(posted_by, message_text, time_posted_epoch) VALUES (?, ?, ?);";
+            String sql = "INSERT INTO message (posted_by, message_text, time_posted_epoch) VALUES (?, ?, ?);";
             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
             preparedStatement.setInt(1, message.getPosted_by());
